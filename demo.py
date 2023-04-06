6ebc5caec55e8b15f7ef158169a47a6a116b97b7
Kyle A McGee <kyle.mcgee34@gmail.com>
2023-04-06 17:01:51 -0400

    commit

--- demo.py
+++ demo.py
@@ -7,13 +7,12 @@ import shutil
 from pathlib import Path
 import ast
 import codecs
+from PIL import Image
 
 dirpath = Path('text_files/')
 if dirpath.exists() and dirpath.is_dir():
     shutil.rmtree(dirpath)
 os.makedirs('text_files/')
-
-url = 'https://ka-darpa-02.chris-mckinley.website'
 
 headers = {
     'CF-Access-Client-Id': st.secrets["clientID"],
@@ -129,6 +128,7 @@ tab1, tab2 = st.tabs(["Create Synthetic Text", "Create Synthetic Image"])
 with tab1:
    '''# Text Generation'''
    '''The model that will be generating text in these examples is gpt2-xl, a 1.5B parameter version of GBT-2 which is a transformer-based language model created and released by OpenAI. The model is a pretrained model on English language using a causal language modeling (CLM) objective.'''
+   url = 'https://ka-darpa-02.chris-mckinley.website'
    col1, col2 = st.columns(2)
    with col1:
       temperature = st.slider('Temperature', min_value = 0.1, max_value = 2.0, step = 0.1, value = 0.5, help = 'Randomness of sampling. High values can increase creativity but may make text less sensible. Lower values will make text more predictable but can become repetitious')
@@ -235,3 +235,23 @@ with tab2:
    with col10:
       if select_prompt == 'Custom Prompt':
          negative_prompt = st.text_area('Enter Negative Prompt')
+         
+   if st.button('Generate Image'):
+      url = "https://sd-darpa-02.chris-mckinley.website"
+      x = requests.post(url=f'{url}/sdapi/v1/options', json=option_payload, headers=headers)
+      payload = {
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "steps": steps,
+        "cfg_scale": cfg_scale,
+        "height": height,
+        "width": width,
+        "sampler_index": sampler_index,
+        "seed": seed
+        }
+      response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload, headers=headers)
+      r = response.json()
+      for i in r['images']:
+         image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+         
+      st.image(image)
